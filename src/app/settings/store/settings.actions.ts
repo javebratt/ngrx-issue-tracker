2a07ba9553dcb699fad9589e4c72f8555a8dcade
Jorge Vergara <jorge@jorgevergara.co>
2022-04-14 21:45:40 -0500

    Adds settings store files

---
+++ src/app/settings/store/settings.actions.ts
@@ -0,0 +1,9 @@
+import { createAction, props } from '@ngrx/store';
+
+export const loadSettingss = createAction(
+  '[Settings] Load Settingss'
+);
+
+
+
+
---
+++ src/app/settings/store/settings.reducer.ts
@@ -0,0 +1,19 @@
+import { Action, createReducer, on } from '@ngrx/store';
+import * as SettingsActions from './settings.actions';
+
+export const settingsFeatureKey = 'settings';
+
+export interface State {
+
+}
+
+export const initialState: State = {
+
+};
+
+export const reducer = createReducer(
+  initialState,
+
+  on(SettingsActions.loadSettingss, state => state),
+
+);
